3635936df65f545d64fabf3eb08b51202c684070
patrickalquimim <134161507+patrickalquimim@users.noreply.github.com>
2023-09-09 10:53:14 -0300

    Update index.js

--- aula6/index.js
+++ aula6/index.js
@@ -12,3 +12,4 @@ anoNascimento = 2023 - idade;
 
 console.log(nome,sobrenome,'tem',idade,'anos','pesa',peso,'kg');
 console.log('tem',alturaEmM,'de altura e seu IMC é de', imc);
+console.log(nome,'nasceu em',anoNascimento,'.');
